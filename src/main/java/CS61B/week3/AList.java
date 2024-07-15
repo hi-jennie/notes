7956de5878b3629c4c52bb02b7c76558bd0862fd
hi-jennie <hi-jennie@outlook.com>
2024-07-15 14:11:13 +0800

    better version of AList

--- src/main/java/CS61B/week3/AList.java
+++ src/main/java/CS61B/week3/AList.java
@@ -5,37 +5,41 @@ package CS61B.week3;
  * AList that can be used to store arbitrarily long lists of data, similar to our DLList.
  * Unlike the DLList, the AList will use arrays to store data instead of a linked list.
  */
+
 public class AList<T> {
-    private static int size;
+    private int size;
     private T[] items;
 
     public AList() {
-        // items = new int[100];
-        // 不能直接用new T[],因为没有T是什么类型，编译器不知道什么意思
         items = (T[]) new Object[100];
         size = 0;
     }
 
     public T get(int i) {
-        return items[i];
+        if (i >= 0 && i < size) {
+            return items[i];
+        } else {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size: " + size);
+        }
     }
 
     public T getLast() {
-        return items[size - 1];
+        if (size > 0) {
+            return items[size - 1];
+        } else {
+            throw new IllegalStateException("List is empty.");
+        }
     }
 
-    public void resize(int i) {
-        T[] a = (T[]) new Object[i];
+    public void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
         System.arraycopy(items, 0, a, 0, size);
         items = a;
     }
 
     public void downSize() {
-        if (size * 4 < items.length) {
-            T[] a = (T[]) new Object[items.length / 2];
-            System.arraycopy(items, 0, a, 0, size);
-            items = a;
-            downSize();
+        while (size * 4 < items.length && items.length > 100) {
+            resize(items.length / 2);
         }
     }
 
@@ -48,29 +52,109 @@ public class AList<T> {
     }
 
     public T remove(int index) {
-        T item = items[index];
-        while (index < size - 1) {
-            items[index] = items[index + 1];
-            index++;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-        // clear the last element
+        T item = items[index];
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
         items[size - 1] = null;
         size--;
         downSize();
-
         return item;
     }
 
-    public void main(String[] args) {
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return items.length;
+    }
+}
+
+class AListDemo {
+    public static void main(String[] args) {
         AList<Integer> aL = new AList<>();
         for (int i = 0; i < 10; i++) {
             aL.addLast(i);
         }
         aL.remove(9);
-        for (int i = 0; i < size; i++) {
-            System.out.println(get(i));
+        for (int i = 0; i < aL.getSize(); i++) {
+            System.out.println(aL.get(i));
         }
 
-        System.out.println(items.length);
+        System.out.println(aL.getCapacity());
     }
 }
+//public class AList<T> {
+//    private int size;
+//    private T[] items;
+//
+//    public AList() {
+//        // items = new int[100];
+//        // 不能直接用new T[],因为没有T是什么类型，编译器不知道什么意思
+//        items = (T[]) new Object[100];
+//        size = 0;
+//    }
+//
+//    public T get(int i) {
+//        return items[i];
+//    }
+//
+//    public T getLast() {
+//        return items[size - 1];
+//    }
+//
+//    public void resize(int i) {
+//        T[] a = (T[]) new Object[i];
+//        System.arraycopy(items, 0, a, 0, size);
+//        items = a;
+//    }
+//
+//    public void downSize() {
+//        if (size * 4 < items.length) {
+//            T[] a = (T[]) new Object[items.length / 2];
+//            System.arraycopy(items, 0, a, 0, size);
+//            items = a;
+//            downSize();
+//        }
+//    }
+//
+//    public void addLast(T item) {
+//        if (size == items.length) {
+//            resize(size * 2);
+//        }
+//        items[size] = item;
+//        size++;
+//    }
+//
+//    public T remove(int index) {
+//        T item = items[index];
+//        for (int i = index; i < size - 1; i++) {
+//            items[i] = items[i + 1];
+//        }
+//        // clear the last element
+//        items[size - 1] = null;
+//        size--;
+//        downSize();
+//
+//        return item;
+//    }
+//
+//    public int getSize() {
+//        return size;
+//    }
+//
+//    public void main(String[] args) {
+//        AList<Integer> aL = new AList<>();
+//        for (int i = 0; i < 10; i++) {
+//            aL.addLast(i);
+//        }
+//        aL.remove(9);
+//        for (int i = 0; i < aL.size; i++) {
+//            System.out.println(get(i));
+//        }
+//
+//        System.out.println(aL.items.length);
+//    }
+//}
