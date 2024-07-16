cee265d78714a7859e0bd78091fc2142bf8901a4
hi-jennie <hi-jennie@outlook.com>
2024-07-16 14:50:32 +0800

    change a SLList to a grid

---
+++ src/main/java/CS61B/week3/SLList.java
@@ -0,0 +1,31 @@
+package CS61B.week3;
+
+public class SLList {
+    Node sentinel;
+
+    public SLList() {
+        this.sentinel = new Node();
+    }
+
+    public int[][] grid(int rows, int cols) {
+        int[][] grid = new int[rows][cols];
+        gridHelper(grid, sentinel.next, 0);
+        return grid;
+    }
+
+    private void gridHelper(int[][] grid, Node curr, int numFilled) {
+        if (curr == sentinel || numFilled >= grid.length * grid[0].length) {
+            return;
+        }
+
+        int row = numFilled / grid[0].length;
+        int col = numFilled % grid[0].length;
+        grid[row][col] = curr.item;
+        gridHelper(grid, curr.next, numFilled + 1);
+    }
+
+    private static class Node {
+        int item;
+        Node next;
+    }
+}
