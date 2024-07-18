7844339ccac7fe8eda51005c10011efb8ce20bc1
hi-jennie <hi-jennie@outlook.com>
2024-07-18 20:43:23 +0800

    comparator

--- src/main/java/CS61B/week4/Dog.java
+++ src/main/java/CS61B/week4/Dog.java
@@ -1,5 +1,8 @@
 package CS61B.week4;
 
+
+import java.util.Comparator;
+
 // Dog class implements Comparable interface and it has to rewrite the compareTo method
 public class Dog implements Comparable<Dog> {
     private final String type;
@@ -8,6 +11,10 @@ public class Dog implements Comparable<Dog> {
     public Dog(String type, int size) {
         this.type = type;
         this.size = size;
+    }
+
+    public static Comparator<Dog> getComparator() {
+        return new NameComparator();
     }
 
     @Override
@@ -28,5 +35,20 @@ public class Dog implements Comparable<Dog> {
         return STR."\{this.getType()} \{this.getSize()}";
 
 
+    }
+
+    public void bark() {
+        System.out.println(this.type + " barkkkkkk");
+    }
+
+    //  convention:we often use get to instantiate to an inner class
+    // instead of Dog.NameComparator nc = new Dog.NameComparator();
+    private static class NameComparator implements Comparator<Dog> {
+        @Override
+        public int compare(Dog a, Dog b) {
+            return Integer.compare(a.size, b.size);
+            // 也可以按照其他的标准排序
+            // return a.type.compareTo(b.type);
+        }
     }
 }
--- src/main/java/CS61B/week4/DogLauncher.java
+++ src/main/java/CS61B/week4/DogLauncher.java
@@ -1,5 +1,7 @@
 package CS61B.week4;
 
+import java.util.Comparator;
+
 public class DogLauncher {
     public static void main(String[] args) {
         Dog d1 = new Dog("Elyse", 3);
@@ -7,7 +9,16 @@ public class DogLauncher {
         Dog d3 = new Dog("Benjamin", 15);
         Dog[] dogs = new Dog[]{d1, d2, d3};
 
-        Dog maxDog = (Dog) Maximizer.max(dogs);
+        Dog maxDog = Maximizer.max(dogs);
         System.out.println(maxDog.toString());
+
+        //  convention:we often use get to instantiate to an inner class
+        // instead of Dog.NameComparator nc = new Dog.NameComparator();
+        Comparator<Dog> nc = Dog.getComparator();
+        if (nc.compare(d1, d2) > 0) {
+            d1.bark();
+        } else {
+            d2.bark();
+        }
     }
 }
