56a0c9a38d763530c42000b5c8f6ad4bd1358b2f
hi-jennie <hi-jennie@outlook.com>
2024-07-25 16:07:13 +0800

    Armstrong number

--- src/main/java/CS61B/week4/Experiment.java
+++ src/main/java/CS61B/week4/Experiment.java
@@ -1,16 +1,5 @@
 package CS61B.week4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Experiment {
 
-
-    public static void main(String[] args) {
-        int[] s = {1, 2, 3,};
-        System.out.println(Arrays.toString(s));
-        ArrayList rawList = new ArrayList();
-        rawList.add("Hello");
-        rawList.add(123);
-    }
 }
---
+++ src/main/java/CS61B/week5/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+package CS61B.week5;
+
+import java.util.ArrayList;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9999; i++) {
+            ArrayList<Integer> pows = new ArrayList<>();
+            int temp = i;
+            while (temp != 0) {
+                int currPow = temp % 10;
+                pows.add(currPow);
+                temp = temp / 10;
+            }
+
+            double sum = 0;
+            for (int j = 0; j < pows.size(); j++) {
+                sum = sum + Math.pow(pows.get(j), pows.size());
+            }
+
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
