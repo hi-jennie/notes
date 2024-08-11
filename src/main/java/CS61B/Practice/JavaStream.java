74fad2d8b53df853ce2fd31fc2931a7bf8e5a347
hi-jennie <hi-jennie@outlook.com>
2024-08-11 21:37:53 +0800

    how to get stream of two-column collection

--- src/main/java/CS61B/Practice/JavaStream.java
+++ src/main/java/CS61B/Practice/JavaStream.java
@@ -1,6 +1,7 @@
 package CS61B.Practice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JavaStream {
     public static void main(String[] args) {
@@ -13,5 +14,19 @@ public class JavaStream {
 
         // print all elements in the list
         list.stream().forEach(letter -> System.out.println(letter));
+
+        // two-column collection
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("a", 1);
+        hm.put("b", 2);
+        hm.put("c", 3);
+        hm.put("d", 4);
+
+        // we need to use keySet() to get the key set of the hashmap,then get the stream
+        hm.keySet().stream().forEach(key -> System.out.println(key));
+
+        hm.entrySet().stream().forEach(entry -> System.out.println(entry));
+
+        
     }
 }
